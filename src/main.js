c8c61b034e58ad29fb281cfaf5bf2edab586009b
hu970804 <812510003@qq.com>
2018-05-06 23:09:01 +0800

    add

--- src/main.js
+++ src/main.js
@@ -1,12 +1,40 @@
-import san from "san";
+import san, { DataTypes } from "san";
 
 var MyApp = san.defineComponent({
-    template: '<p>Hello {{name}}!</p>',
+    template: 
+        `
+            <div>
+                <div>
+                    <input name="name" type="text" placeholder="姓名" value="{= name =}">
+                    <input name="age" type="number" placeholder="年龄" value="{= age =}" on-change="changeNumber(age)">
+                    <input name="des" type="text" placeholder="简介" value="{= des =}">
+                </div>
+                <div><button on-click="remove">移除信息</button></div>
+                <label for="">姓名：{{name}}</label>
+                <label for="">年龄：{{age}}</label>
+                <label for="">简介：{{des}}</label>
+            </div>
+        `,
 
     initData: function () {
         return {
-            name: 'San'
+            name: "",
+            age: null,
+            des: ""
         };
+    },
+    dataTypes: {
+        name: DataTypes.string,
+        age: DataTypes.number,
+        des: DataTypes.string
+    },
+    remove: function() {
+        this.data.set('name', "");
+        this.data.set('age', null);
+        this.data.set('des', "")
+    },
+    changeNumber: function (age) {
+        this.data.set("age", parseInt(age))
     }
 });
 
